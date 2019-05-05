6631b9761602e2668342be2c7922921e54122b64
kornel43 <kornel43@gmail.com>
2019-05-06 00:06:09 +0200

    Added more info about things needed to encrypt point

--- lab5/main.py
+++ lab5/main.py
@@ -13,12 +13,12 @@ if __name__ == '__main__':
     d = 5
     p = 17
     ec = EdwardsCurves(d, p)
-    g = ec.create_point(10, 5)
-    message = ec.create_point(12, 7)
+    g = ec.create_point(10, 5)  # must be on edwards curve
+    message = ec.create_point(12, 7)  # must be on edwards curve
     eg = ElGamal(ec, g)
     priv_key = random.randint(1, eg.n - 1)  # smaller than order of point because we don't want pub_key to be base_point
-    rand_int = random.randint(1, eg.n - 1)
-    pub_key = ec.scalar_mul(priv_key, g)
+    rand_int = random.randint(1, eg.n - 1)  # does not have to be smaller than order of g
+    pub_key = ec.scalar_mul(priv_key, g)  # must be on edwards curve
     encoded = eg.encrypt_point(message, pub_key, rand_int)
     decrypted = eg.decrypt_cipher(encoded, priv_key)
     assert message == decrypted
